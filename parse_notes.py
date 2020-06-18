8b3b3751036ed0ee0c2b5ca57d2827f22a18b314
xylnx <xylynx@mailbox.org>
2020-06-18 12:59:01 +0200

    Rewrite how questions and answers are parsed

--- parse_notes.py
+++ parse_notes.py
@@ -9,8 +9,8 @@ import sys, getopt
 import re
 import time
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+# print('Number of arguments:', len(sys.argv), 'arguments.')
+# print('Argument List:', str(sys.argv))
 
 file_path= str(sys.argv[1])
 input_file= str(sys.argv[2])
@@ -21,46 +21,16 @@ md_input= file_path + '/' + input_file
 html_output= file_path + '/' + export_file
 
 # Define constant to signal end of a question field
-END_OF_QUESTION_STRING = '%eq'
+BEGINN_QUESTION = 'h3'
+END_QUESTION = '%eq'
 
 # List classes to be removed
 REMOVE_ATTRIBUTES = [
   'class', 'alt'
 ]
 
-# Count h3 and h4 tags to determine num of questions
-tag_count = 0
-
-
-# Return current date formatted as 2020-05-31
-def current_date():
-    seconds = time.time()
-    structured_time = time.localtime(seconds)
-    year = structured_time.tm_year
-    month = (structured_time.tm_mon
-              if len(str(structured_time.tm_mon)) == 2
-              else '0{}'.format(structured_time.tm_mon))
-    day = (structured_time.tm_mday
-            if len(str(structured_time.tm_mday)) == 2
-            else '0{}'.format(structured_time.tm_mday))
-
-    return ("%s-%s-%s" % (year, month, day))
-
-def current_time():
-  seconds = time.time()
-  structured_time= time.localtime(seconds)
-  hour = (structured_time.tm_hour
-          if len(str(structured_time.tm_hour)) == 2
-          else '0{}'.format(structured_time.tm_hour))
-  minutes = (structured_time.tm_min
-              if len(str(structured_time.tm_min)) == 2
-              else '0{}'.format(structured_time.tm_min))
-  seconds = (structured_time.tm_sec
-              if len(str(structured_time.tm_sec)) == 2
-              else '0{}'.format(structured_time.tm_sec))
-
-  return ("%s:%s:%s" % (hour, minutes, seconds))
-
+# Count questions
+qestion_count = 0
 
 # Open markdown file
 with open(md_input, "r", encoding="utf-8") as input_file:
@@ -112,6 +82,15 @@ for quot_symbol in quot_symbols:
 # Format html so it can be read by Anki's importer #
 ###################################################
 
+# Ankis importer reads text files and excepts HTML in them.
+# Here we use a simple card layout: 2 Fields --> front (question) and back (answer)
+# Anki distinguishes fields through a separator (semicolon, comma etc.)
+# Here we use a tab as separator
+# A new line is needed to signal the beginning of a new card
+# Every question and every answer is wrapped in quotes
+# This allows for multi line questions and answers (and new lines in questions and answers, which would otherwise be identified as beginnings of new cards)
+
+
 # Get rid of all new lines by minifying html
 html_min = htmlmin.minify(str(soup))
 # Strip whitespace
@@ -119,46 +98,107 @@ html_min = html_min.strip()
 # Cook fresh soup
 soup = BeautifulSoup(html_min, "html.parser")
 
-# Grab all h3 tags from soup
-h3_tags = soup.find_all('h3')
 
-# Identify the end of a question and beginning of an answer:
-# add tab after every h3 closing tag (end of a question)
-# add quotation mark to identify begin of answer
-for h3 in h3_tags:
-  h3.insert_after('\t"')
+# Grab all begin and end tags from soup
+begin_tags = soup.find_all(BEGINN_QUESTION)
 
+# Determine if there is an end_tag in between two begin_tags
+# If true: no action required
+# False : insert answer and question separator directly after begin_tag
+is_end_tag = False
+
+for begin_tag in begin_tags:
+  loop_variable = begin_tag
+  while (loop_variable.find_next_sibling() and loop_variable.find_next_sibling().name != 'h3'):
+    if (loop_variable.find_next_sibling().text == END_QUESTION):
+      is_end_tag = True
+      break
+      loop_variable = loop_variable.find_next_sibling()
+    else:
+      loop_variable = loop_variable.find_next_sibling()
+  
+  if (not is_end_tag):
+    begin_tag.insert_after('"\t"')
+    print('insert after begin_tag')
+    qestion_count += 1
+  
+  is_end_tag = False
+
+end_tags = soup.find_all('p', string=END_QUESTION)
+# paragraphs = soup.find_all('p')
+
+# Mark beginning of a question
+# no new line for first question
+begin_tags[0].insert_before('"')
+
+# Close answer from previous question (")
+# Mark beging of next question (\t)
+# Start wrapping the question(")
+for begin_tag in begin_tags[1:]:
+  begin_tag.insert_before('"\n"')
+
+# Close question (")
+# Insert tab separator (\t)
+# Start wrapping answer ("")
+for end_tag in end_tags:
+  end_tag.insert_before('"\t"')
   # Increment no of questions
-  # this counts all questions marked with an h3 tag
-  tag_count += 1
+  qestion_count += 1
+
+  # Remove end of question identifier
+  end_tag.extract()
+
+# Mark end of last question
+# get all tags
+tags = soup.findAll()
+# get last html element
+t = tags[-1]
+
+# Problem: html elements are nested
+# Solution: get last parent of last element before root
+while (t.parent.name != "[document]"):
+  t = t.parent
+
+# insert quote after last parent of last element
+t.insert_after('"')
+
+
+
+    
+
+# if (tags[-1].name == 'code'):
+#   pass
+
+# tags[-1].insert_after('"')
+
+
 
 # Identify the end of an answer and beginning of a question
 # add quotation mark to identify the end of an answer
 # add new line before h3 openeing tags to identify beginn next question
 # Do this except for first h3 tag (first question has no previous answer)
-for h3 in h3_tags[1:]:
-  h3.insert_before('"\n')
+
 
 
 # Workaround for multiple choice questions
 ##########################################
-h4_tags = soup.find_all('h4')
+# h4_tags = soup.find_all('h4')
 
-for h4 in h4_tags:
-  h4.insert_before('"\n')
+# for h4 in h4_tags:
+#   h4.insert_before('"\n')
 
-# Use string to identify the end of question, instead of closing tag
-end_of_question_tags = soup.find_all('p', string=END_OF_QUESTION_STRING)
+# # Use string to identify the end of question, instead of closing tag
+# end_of_question_tags = soup.find_all('p', string=END_QUESTION)
 
-# Insert a tab after end of question identifier (<p>%eq</p> ->)
-# Add quotation mark to identify begin answer (<p>%eq</p> ->")
-for end_of_question_tag in end_of_question_tags:
-  end_of_question_tag.insert_after('\t"')
-  # Remove end of question identifier (->")
-  end_of_question_tag.extract()
+# # Insert a tab after end of question identifier (<p>%eq</p> ->)
+# # Add quotation mark to identify begin answer (<p>%eq</p> ->")
+# for end_of_question_tag in end_of_question_tags:
+#   end_of_question_tag.insert_after('\t"')
+#   # Remove end of question identifier (->")
+#   end_of_question_tag.extract()
   
-  # Increment tag_count (to count no of questions)
-  tag_count += 1
+#   # Increment qestion_count (to count no of questions)
+#   qestion_count += 1
 
 ###################################
 ###################################
@@ -178,7 +218,7 @@ os.system('cd ' + file_path + ' && cp *.jpg /Users/jep/Library/Application\ Supp
 # Print number of questions
 print('#########################')
 
-print('# No. of questions: ' + str(tag_count) + ' #')
+print('# No. of questions: ' + str(qestion_count) + ' #')
 
 print('#########################')
 
